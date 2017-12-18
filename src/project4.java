0c31f8522d8d336e5469ac0b0112b431fea9d93b
raghibboss <yeasinarafat19082@gmail.com>
2017-12-18 03:04:25 -0500

    this is my frist commit

---
+++ src/project4.java
@@ -0,0 +1,22 @@
+
+public class project4 {
+
+	public static void main(String[] args) {
+		char ch;
+
+		//printing uppercase alphabets 
+		System.out.println("Uppercase alphabets:");
+		for(ch='A';ch<='Z';ch++)
+			System.out.print(ch + " ");
+
+		//printing new line
+		System.out.println("");
+
+		//printing lowercase alphabets 
+		System.out.println("Lowercase alphabets:");
+		for(ch='a';ch<='z';ch++)
+			System.out.print(ch +"");
+
+	}
+
+}
---
+++ src/project8scanner.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class project8scanner {
+public static void main (String arg[]){
+	Scanner yeasin =new Scanner(System.in);
+	System.out.println(yeasin.nextLine());
+}
+
+}
